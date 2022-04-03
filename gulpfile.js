a6e0422fd19e652ea08484af43fabe0d5785409a
Your Name <you@example.com>
2022-04-03 18:47:11 +0300

    8888

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,26 @@ const del = require("del");
 const browserSync = require("browser-sync").create();
 const svgo = require("gulp-svgo");
 const svgSprite = require("gulp-svg-sprite");
+const fileInclude = require("gulp-file-include");
+
+const htmlInclude = () => {
+  return src(["app/html/*.html"]) // Находит любой .html файл в папке "html", куда будем подключать другие .html файлы
+    .pipe(
+      fileInclude({
+        prefix: "@",
+        basepath: "@file",
+      })
+    )
+    .pipe(dest("app")) // указываем, в какую папку поместить готовый файл html
+    .pipe(browserSync.stream());
+};
 
 function scripts() {
-  return src(["node_modules/jquery/dist/jquery.js",
+  return src([
+    "node_modules/jquery/dist/jquery.js",
     "node_modules/slick-carousel/slick/slick.js",
-    "app/js/main.js"])
+    "app/js/main.js",
+  ])
     .pipe(concat("main.min.js"))
     .pipe(uglify())
     .pipe(dest("app/js"))
@@ -25,7 +40,9 @@ function svg() {
       svgo({
         plugins: [
           {
-            removeAttrs: { attrs: "(fill|srtoke|data.*)" },
+            removeAttrs: {
+              attrs: "(fill|srtoke|data.*)",
+            },
           },
         ],
       })
@@ -108,12 +125,14 @@ function cleanDist() {
 }
 
 function watching() {
+  watch(["app/html/**/*.html"], htmlInclude);
   watch(["app/images/icons/**/*.svg"], svg);
   watch(["app/scss/**/*.scss"], styles);
   watch(["app/js/**/*.js", "!app/js/main.min.js"], scripts);
   watch(["app/**/*.html"]).on("change", browserSync.reload);
 }
 
+exports.htmlInclude = htmlInclude;
 exports.styles = styles;
 exports.scripts = scripts;
 exports.browsersync = browsersync;
@@ -123,6 +142,7 @@ exports.svg = svg;
 exports.build = series(cleanDist, tinypng, build);
 
 exports.default = parallel(
+  htmlInclude,
   styles,
   svg,
   scripts,
