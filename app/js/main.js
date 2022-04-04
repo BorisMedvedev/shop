fe660c219839d45e7aba789930495565eb3e75f1
Your Name <bmedved78@mail.ru>
2022-04-04 14:13:03 +0300

    add cards

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,20 @@
-
 $(function () {
 
   $('.top-slider__list').slick({
     dots: true,
     arrows: false,
     fade: true,
-    autoplay: true,
-    autoplaySpeed: 3000
+    // autoplay: true,
+    // autoplaySpeed: 3000
 
   });
 
   $(".rateyo").rateYo({
-    starWidth: "40px"
+    starWidth: "16px",
+    "rating": 2.2,
+    spacing: "2px",
+    ratedFill: "#ffb534",
+    normalFill: "#8d8d8d"
   }
 
   );
