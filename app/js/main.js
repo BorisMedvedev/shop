1b05819ba8dea6c8f206c9b770ec6ddbbf7faee5
Your Name <bmedved78@mail.ru>
2022-04-04 16:28:38 +0300

    add video

--- app/js/main.js
+++ app/js/main.js
@@ -21,3 +21,14 @@ $(function () {
 
 
 });
+
+
+// //Закрываем окно и слой затемнения
+// $(document).on('click', 'a.close, #fade', function () { //закрытие по клику вне окна и кнопке закрытия...
+//   $('#fade , .popup_block').fadeOut(function () {
+//     $('#fade, a.close').remove(); //При нажатии закрывается окно и видео останавливается
+//     document.location.reload();
+//   });
+//   return false;
+// });
+// });
