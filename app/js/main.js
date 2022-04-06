0fdf63b4b15327f5ce8d66f1acb1060940276381
Your Name <bmedved78@mail.ru>
2022-04-06 15:36:58 +0300

    777

--- app/js/main.js
+++ app/js/main.js
@@ -41,6 +41,20 @@ $(function () {
 
   });
 
+  $(".tab").on("click", function (e) {
+    e.preventDefault();
+
+    $($(this).siblings()).removeClass("tab--active");
+
+    $($(this).closest('.tabs-wrapper')).siblings().find("div").removeClass("tabs-content--active");
+
+
+    $(this).addClass("tab--active");
+    $($(this).attr("href")).addClass("tabs-content--active");
+    $('.product-slider').slick('setPosition');
+
+  });
+
 });
 
 function getTimeRemaining(endtime) {
